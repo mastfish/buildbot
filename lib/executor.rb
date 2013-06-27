f289800c4281a758d0731ba88fc1f20eca1669bd
Justin Lambert <justin.lambert@bigcommerce.com>
2013-06-27 17:36:11 +1000

    Well, we can always just brute-force the required fields...

--- lib/executor.rb
+++ lib/executor.rb
@@ -7,12 +7,12 @@ class Executor
   require 'active_record'
   require 'rest_client'
 
+  API_BASE = "https://justin.lambert:#{ENV['PASSWORD']}@bamboo.bigcommerce.net/"
+
   ActiveRecord::Base.establish_connection(
     :adapter => 'sqlite3',
     :database => 'buildbot_db'
   )
-
-  R = RestClient # I'm hella lazy
 
   class PullLog < ActiveRecord::Base
   end
@@ -22,18 +22,7 @@ class Executor
   #end test
 
   def main
-    url ="https://justin.lambert:#{ENV['PASSWORD']}@bamboo.bigcommerce.net/rest/api/latest/clone/BUILDBOT-MAIN:BUILDBOT-FIRST?os_authType=basic"
-    begin
-      req = RestClient::Request.new(
-        :method => :put,
-        :url => url,
-        :headers => { :accept => 'application/json',
-        :content_type => 'application/json' }
-      ).execute
-      binding.pry
-    rescue => e
-      binding.pry
-    end
+    update_info
     # BuildQueue.where(locked_at: nil).each do |build_trigger|
     #   trigger_build build_trigger
     # end
@@ -41,7 +30,77 @@ class Executor
 
   def trigger_build build_trigger
     pull = PullLog.where(pull_id: build_trigger.pull_id)
-    #
+  end
+
+  def clone
+    url = API_BASE + "rest/api/latest/clone/BUILDBOT-MAIN:BUILDBOT-FIRST?os_authType=basic"
+    req = RestClient::Request.new(
+      :method => :put,
+      :url => url,
+      :headers => { :accept => 'application/json',
+      :content_type => 'application/json' }
+    ).execute
+  end
+
+  def update_info
+    payload = test_payload
+    url = API_BASE + "chain/admin/config/updateRepository.action"
+    req = RestClient::Request.new(
+      :method => :post,
+      :url => url,
+      :headers => { :accept => 'text/html'},
+      :payload => payload,
+      :multipart => true
+    ).execute
+    binding.pry
+  end
+
+  def test_payload
+    {
+      "PlanKey" => "BUILDBOT-FIRST",
+      "repositoryId" => "83132573",
+      "selectedRepository" => "com.atlassian.bamboo.plugins.atlassian-bamboo-plugin-git\":gh",
+      "selectFields" => "selectedRepository",
+      "repositoryName" => "GitHub",
+      "repository.github.username" => "mastfish",
+      "temporary.github.password.change" => "true",
+      "repository.github.temporary.password" => "",
+      "repository.github.repository" => "mastfish/buildbot",
+      "selectFields" => "repository.github.repository",
+      "repository.github.branch" => "dev",
+      "selectFields" => "repository.github.branch",
+      "repository.github.useShallowClones" => "true",
+      "checkBoxFields" => "repository.github.useShallowClones",
+      "checkBoxFields" => "repository.github.useSubmodules",
+      "repository.github.commandTimeout" => "180",
+      "checkBoxFields" => "repository.github.verbose.logs",
+      "checkBoxFields" => "repository.common.quietPeriod.enabled",
+      "repository.common.quietPeriod.period" => "10",
+      "repository.common.quietPeriod.maxRetries" => "5",
+      "filter.pattern.option" => "none",
+      "selectFields" => "filter.pattern.option",
+      "filter.pattern.regex" => "",
+      "changeset.filter.pattern.regex" => "",
+      "selectedWebRepositoryViewer" => "bamboo.webrepositoryviewer.provided\":noRepositoryViewer",
+      "selectFields" => "selectedWebRepositoryViewer",
+      "webRepository.genericRepositoryViewer.webRepositoryUrl" => "",
+      "webRepository.genericRepositoryViewer.webRepositoryUrlRepoName" => "",
+      "webRepository.stash.url" => "",
+      "webRepository.stash.project" => "",
+      "webRepository.stash.repositoryName" => "",
+      "webRepository.fisheyeRepositoryViewer.webRepositoryUrl" => "",
+      "webRepository.fisheyeRepositoryViewer.webRepositoryRepoName" => "",
+      "webRepository.fisheyeRepositoryViewer.webRepositoryPath" => "",
+      "webRepository.hg.scheme" => "bitbucket",
+      "selectFields" => "webRepository.hg.scheme",
+      "bamboo.successReturnMode" => "json-as-html",
+      "decorator" => "nothing",
+      "confirm" => "true",
+    }
+  end
+
+  def send_build_request
+
   end
 
 end
